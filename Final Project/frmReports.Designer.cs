81b2f2cf198ba80f5991a7163fa0a87209c8b3dc
Andrew Lyttle <andrew.lyttle@hotmail.com>
2025-02-01 17:39:50 +0000

    Experimenting with options for reports

--- Final Project/frmReports.Designer.cs
+++ Final Project/frmReports.Designer.cs
@@ -28,17 +28,80 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReports));
 			pnlOrdersReport = new Panel();
+			printPreviewControl1 = new PrintPreviewControl();
+			btnPreviewReport = new Button();
+			btnOpenPDF = new Button();
+			printDocument1 = new System.Drawing.Printing.PrintDocument();
+			printPreviewDialog1 = new PrintPreviewDialog();
+			btnPrintReport = new Button();
+			pnlOrdersReport.SuspendLayout();
 			SuspendLayout();
 			// 
 			// pnlOrdersReport
 			// 
 			pnlOrdersReport.BackColor = SystemColors.ScrollBar;
+			pnlOrdersReport.Controls.Add(btnPrintReport);
+			pnlOrdersReport.Controls.Add(printPreviewControl1);
+			pnlOrdersReport.Controls.Add(btnPreviewReport);
+			pnlOrdersReport.Controls.Add(btnOpenPDF);
 			pnlOrdersReport.Dock = DockStyle.Top;
 			pnlOrdersReport.Location = new Point(0, 0);
 			pnlOrdersReport.Name = "pnlOrdersReport";
 			pnlOrdersReport.Size = new Size(1000, 680);
 			pnlOrdersReport.TabIndex = 0;
+			// 
+			// printPreviewControl1
+			// 
+			printPreviewControl1.Location = new Point(27, 220);
+			printPreviewControl1.Name = "printPreviewControl1";
+			printPreviewControl1.Size = new Size(909, 347);
+			printPreviewControl1.TabIndex = 3;
+			// 
+			// btnPreviewReport
+			// 
+			btnPreviewReport.Location = new Point(393, 34);
+			btnPreviewReport.Name = "btnPreviewReport";
+			btnPreviewReport.Size = new Size(258, 100);
+			btnPreviewReport.TabIndex = 2;
+			btnPreviewReport.Text = "Preview";
+			btnPreviewReport.UseVisualStyleBackColor = true;
+			btnPreviewReport.Click += btnPreviewReport_Click;
+			// 
+			// btnOpenPDF
+			// 
+			btnOpenPDF.Location = new Point(27, 30);
+			btnOpenPDF.Name = "btnOpenPDF";
+			btnOpenPDF.Size = new Size(248, 109);
+			btnOpenPDF.TabIndex = 0;
+			btnOpenPDF.Text = "Open Report";
+			btnOpenPDF.UseVisualStyleBackColor = true;
+			btnOpenPDF.Click += btnOpenPDF_Click;
+			// 
+			// printDocument1
+			// 
+			printDocument1.PrintPage += printDocument1_PrintPage;
+			// 
+			// printPreviewDialog1
+			// 
+			printPreviewDialog1.AutoScrollMargin = new Size(0, 0);
+			printPreviewDialog1.AutoScrollMinSize = new Size(0, 0);
+			printPreviewDialog1.ClientSize = new Size(400, 300);
+			printPreviewDialog1.Enabled = true;
+			printPreviewDialog1.Icon = (Icon)resources.GetObject("printPreviewDialog1.Icon");
+			printPreviewDialog1.Name = "printPreviewDialog1";
+			printPreviewDialog1.Visible = false;
+			// 
+			// btnPrintReport
+			// 
+			btnPrintReport.Location = new Point(683, 34);
+			btnPrintReport.Name = "btnPrintReport";
+			btnPrintReport.Size = new Size(253, 100);
+			btnPrintReport.TabIndex = 4;
+			btnPrintReport.Text = "Print";
+			btnPrintReport.UseVisualStyleBackColor = true;
+			btnPrintReport.Click += btnPrintReport_Click;
 			// 
 			// frmReports
 			// 
@@ -50,11 +113,18 @@
 			Font = new Font("Segoe UI", 14F);
 			Name = "frmReports";
 			Text = "Reports";
+			pnlOrdersReport.ResumeLayout(false);
 			ResumeLayout(false);
 		}
 
 		#endregion
 
 		private Panel pnlOrdersReport;
+		private Button btnOpenPDF;
+		private Button btnPreviewReport;
+		private System.Drawing.Printing.PrintDocument printDocument1;
+		private PrintPreviewDialog printPreviewDialog1;
+		private PrintPreviewControl printPreviewControl1;
+		private Button btnPrintReport;
 	}
 }
--- Final Project/frmReports.cs
+++ Final Project/frmReports.cs
@@ -3,18 +3,84 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
 
 namespace Final_Project
 {
 	public partial class frmReports : Form
 	{
+
 		public frmReports()
 		{
 			InitializeComponent();
+		}
+
+		private void btnOpenPDF_Click(object sender, EventArgs e)
+		{
+			string program = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+			//string filename = @"C:\html.html";
+			string filename = @"C:\Users\andre\Downloads\EXEMPLAR_9.pdf";
+			System.Diagnostics.Process.Start(program, filename);
+		}
+
+		private string documentContents = "";
+		private string stringToPrint = "";
+		private void btnPreviewReport_Click(object sender, EventArgs e)
+		{
+			string docName = "html.html";
+			string docPath = @"C:\";
+			string fullPath = System.IO.Path.Combine(docPath, docName);
+			printDocument1.DocumentName = docName;
+			stringToPrint = System.IO.File.ReadAllText(fullPath);
+			stringToPrint = "This is my report\nWith some more lines";
+			//printPreviewDialog1.Document = printDocument1;
+			//printPreviewDialog1.ShowDialog();
+			printPreviewControl1.Document = printDocument1;
+		}
+
+		private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+		{
+
+			int charactersOnPage = 0;
+			int linesPerPage = 0;
+
+			// Sets the value of charactersOnPage to the number of characters
+			// of stringToPrint that will fit within the bounds of the page.
+			e.Graphics.MeasureString(stringToPrint, this.Font,
+				e.MarginBounds.Size, StringFormat.GenericTypographic,
+				out charactersOnPage, out linesPerPage);
+
+			// Draws the string within the bounds of the page.
+			e.Graphics.DrawString(stringToPrint, this.Font, Brushes.Black,
+			e.MarginBounds, StringFormat.GenericTypographic);
+
+			// Remove the portion of the string that has been printed.
+			stringToPrint = stringToPrint.Substring(charactersOnPage);
+
+			// Check to see if more pages are to be printed.
+			e.HasMorePages = (stringToPrint.Length > 0);
+
+			// If there are no more pages, reset the string to be printed.
+			if (!e.HasMorePages)
+				stringToPrint = documentContents;
+		}
+
+		private void btnPrintReport_Click(object sender, EventArgs e)
+		{
+			string docName = "html.html";
+			string docPath = @"C:\";
+			string fullPath = System.IO.Path.Combine(docPath, docName);
+			printDocument1.DocumentName = docName;
+			stringToPrint = System.IO.File.ReadAllText(fullPath);
+			stringToPrint = "This is my report\nWith some more lines";
+			printPreviewDialog1.Document = printDocument1;
+			printPreviewDialog1.ShowDialog();
 		}
 	}
 }
